a53c24d943b6896ebcdf35bff6f97bcd6756c761
alan <alan@compendiumdev.co.uk>
2023-07-19 18:32:50 +0100

    try with an action

---
+++ src/test/java/com/eviltester/webdriver/MyFirstChromeTest.java
@@ -0,0 +1,57 @@
+package com.eviltester.webdriver;
+
+//import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class MyFirstChromeTest {
+
+    /**
+     The following code is for the Chrome Driver.
+    */
+
+    @Test
+    public void startWebDriver(){
+
+        //WebDriverManager.chromedriver().setup();
+
+        ChromeOptions options = new ChromeOptions();
+        if(System.getenv().
+                getOrDefault("BROWSER_STATE","show").
+                equals("Headless")){
+            options.addArguments("headless");
+        }
+
+        //options.addArguments("--remote-allow-origins=*");
+
+        WebDriver driver = new ChromeDriver(options);
+
+        driver.navigate().to("https://testpages.herokuapp.com");
+
+        Assertions.assertTrue(
+                        driver.getTitle().startsWith("Selenium"),
+                        "title should start with Selenium");
+
+        driver.close();
+        driver.quit();
+    }
+
+    /*
+
+     if https://testpages.herokuapp.com is not working then you can download the
+     test pages app from github
+
+     https://github.com/eviltester/TestingApp/tree/master/java/testingapps/seleniumtestpages
+
+     The herokuapp and github release are maintained.
+
+     If you want to learn more about Selenium WebDriver then check out my online courses:
+
+     https://eviltester.com/courses
+
+     */
+
+}
